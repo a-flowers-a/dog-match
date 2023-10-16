da3fc8c8fc4e60508544693f234ec9c5f44b3b6b
a-flowers-a <aalvarezf1601@alumno.ipn.mx>
2023-10-15 20:35:43 -0600

    authenticate and move to Home

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,41 @@
+import { useNavigate } from "react-router-dom";
 //COmponents
 import { useCallback } from "react";
 import LoginCard from "../../components/molecules/LoginCard";
 import { LoginData } from "../../types/authentication";
+//Context
+import { AuthContext } from "../../context/AuthProvider/context";
 //Services
 import { login } from "../../services/authentication";
+//Types & Consts
+import { RoutePath } from "../../constants/routes";
 //Styles
 import "../../globalStyles/shared.scss";
 import "./styles.scss";
 
 function SignIn() {
+  //Context
+  const {
+    actions: { setAuthState },
+  } = AuthContext();
+
+  const navigate = useNavigate();
+
   /**
    * Calls service to perform login and moves to the Home page
    */
-  const handleLogin = useCallback(async (loginData: LoginData) => {
-    try {
-      await login(loginData);
-      console.log("successful login");
-    } catch (error) {
-      console.log("error at handleLogin", error);
-    }
-  }, []);
+  const handleLogin = useCallback(
+    async (loginData: LoginData) => {
+      try {
+        await login(loginData);
+        setAuthState({ isAuthenticated: true });
+        navigate(RoutePath.Home);
+      } catch (error) {
+        console.log("error at handleLogin", error);
+      }
+    },
+    [navigate, setAuthState]
+  );
 
   return (
     <div className="base-page sign-in-container">
